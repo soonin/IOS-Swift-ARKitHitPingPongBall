df45230c593c018c0609d64f77bd14acc91329b2
Pooya Hatami <pooya@node-1w7jr9qrok3iazh7zxfp47ti4.ipv6.telus.net>
2018-03-22 23:11:01 -0700

    Ready!

--- IOS-Swift-ARKitHitPingPongBall/ViewController.swift
+++ IOS-Swift-ARKitHitPingPongBall/ViewController.swift
@@ -13,6 +13,9 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var ball = SCNNode()
+    var box = SCNNode()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +25,85 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        //sceneView.allowsCameraControl = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/MainScene.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let wait:SCNAction = SCNAction.wait(duration: 3)
+        let runAfter:SCNAction = SCNAction.run { _ in
+            
+            self.addSceneContent()
+        }
+        let seq:SCNAction = SCNAction.sequence([wait, runAfter])
+        sceneView.scene.rootNode.runAction(seq)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func handleTap( sender: UITapGestureRecognizer){
+        
+       // guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, options: [:])
+        if !hitTestResult.isEmpty {
+            for hitResult in hitTestResult {
+                
+                //print(hitResult.node.name)
+                
+                if (hitResult.node == ball) {
+                    
+                    ball.physicsBody?.applyForce(SCNVector3(0,2,0), asImpulse: true)
+                    
+                } else if hitResult.node == box {
+                    
+                    ball.position = SCNVector3(0, 5 , 1 )
+                    
+                }
+            }
+        }
+        
+    }
+    
+    
+    func addSceneContent(){
+        let dummyNode = self.sceneView.scene.rootNode.childNode(withName: "DummyNode", recursively: false)
+        dummyNode?.position = SCNVector3(0 , -5 , -5)
+        
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if ( node.name == "ball" ) {
+                print("Found Ball")
+                
+                ball = node
+                ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "ballPatternTexture01")
+                ball.physicsBody = SCNPhysicsBody(type: .dynamic , shape: SCNPhysicsShape(node: ball, options: nil))
+                ball.physicsBody?.isAffectedByGravity = true
+                ball.physicsBody?.restitution = 1
+                
+            } else if ( node.name == "box") {
+                
+                print("Found Box")
+                
+                box = node
+                box.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "PPTableGreen01")
+                let boxGeometory = box.geometry
+                let boxShape: SCNPhysicsShape = SCNPhysicsShape(geometry: boxGeometory!, options: nil)
+                box.physicsBody = SCNPhysicsBody(type: .static , shape: boxShape)
+                box.physicsBody?.restitution = 1
+            }
+        }
+        
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.omni
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3(x: 1.5 , y: 1.5 , z: 1.5)
+        self.sceneView.scene.rootNode.addChildNode(lightNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +111,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+       
+        // self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints, SCNDebugOptions.showPhysicsShapes ]
+        
+       // configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
